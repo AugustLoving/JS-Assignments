9e6aec57ac6e60c28b9b3d5895bc3c5a49196cdd
AugustLoving <auglov536@ihmeducation.se>
2024-09-12 15:20:11 +0200

    uppgifter gjorda page 1

---
+++ P5.1-uppgifter/js/app.js
@@ -0,0 +1,36 @@
+// kmh / 3,6 = m/2
+let kmh = 72;
+const MeterSekund = 3.6;
+
+console.log(kmh /= MeterSekund);
+
+let speedinput = 72;
+let speedoutput = 0;
+speedoutput = speedinput / 3.6;
+console.log(speedoutput);
+
+let minutesinput = 3;
+let secondsoutput = 0;
+secondsoutput = minutesinput * 60;
+
+console.log(secondsoutput);
+
+
+let numberinput = 11;
+let divisionoutput = 0;
+divisionoutput = numberinput / 4;
+
+console.log(divisionoutput);
+
+let input = 11;
+let output = 0;
+output = input % 4;
+console.log(output);
+
+let moduloinput = 4
+let modulooutput = 0;
+modulooutput = moduloinput % 7;
+
+console.log(modulooutput);
+
+
