3633ede5fbd066a317c7be649c62b171b68e9236
AugustLoving <auglov536@ihmeducation.se>
2024-09-12 13:43:28 +0200

    Update app.js

---
+++ P5-operators/js/app.js
@@ -0,0 +1,21 @@
+let x = 5 + 5;
+let y = x + 5;
+let z = y + y;
+console.log(z + z);
+
+let firstnumber = 1;
+let secondanswer = 2;
+let answer = 0;
+
+answer = firstnumber + secondanswer
+console.log(firstnumber + secondanswer);
+
+answer = firstnumber - secondanswer;
+console.log(answer);
+
+answer = firstnumber * secondanswer;
+console.log(answer);
+
+answer = firstnumber / secondanswer;
+console.log(answer);
+
