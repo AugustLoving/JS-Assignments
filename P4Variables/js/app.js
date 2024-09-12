af0f6f45d647d01013bf8dd002a338238df0c1ea
AugustLoving <auglov536@ihmeducation.se>
2024-09-12 12:59:27 +0200

    Update app.js

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,6 +1,7 @@
 let numberofbottles = 10
 const line1 = "green bottles hanging on the wall";
 const line2 = "and if one bottle, should accidentally fall, there will be";
+while (numberofbottles > 0) {}
 
 console.log(numberofbottles);
 console.log(line1);
