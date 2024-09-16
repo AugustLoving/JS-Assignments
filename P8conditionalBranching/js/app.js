8c91b2b5e97cf00e72d2077c80e492a104bcc1a4
AugustLoving <auglov536@ihmeducation.se>
2024-09-16 13:41:03 +0200

    add even odd

---
+++ P8conditionalBranching/js/app.js
@@ -0,0 +1,51 @@
+const birthYear = 2000;
+const currentYear = 2024;
+const favoriteNumber = 21;
+
+let ageOfPerson = currentYear - birthYear;
+console.log("your age is " + ageOfPerson);
+
+// Save to a let variable named isChild, when the age is from 0 till 10.
+let isChild = (ageOfPerson >= 0) && (ageOfPerson <= 10);
+
+// Save to a let variable named isTeenager, when the age is from 11 to 17
+let isTeenager = (ageOfPerson >= 11) && (ageOfPerson <= 17);
+
+// Save to a let variable named isAdult, when the age is 18 or more.
+let isAdult =  ageOfPerson >= 18;
+
+if (isChild) {
+  console.log("You are a child.");
+}
+
+if (isTeenager) {
+  console.log("You are a teenager.");
+}
+
+if (isAdult) {
+  console.log("You are an adult.");
+}
+
+console.log("your favorite number is " + favoriteNumber);
+
+if (favoriteNumber > ageOfPerson) {
+  console.log("favorite number is greater");
+}
+
+else if (favoriteNumber === ageOfPerson) {
+  console.log("they are the same");
+}
+
+else {
+  console.log("Age is greater");
+}
+
+
+if (favoriteNumber % 2 === 0) {
+  console.log("your favorite number is even");
+}
+
+else {
+  console.log("favorite number is odd");
+}
+
