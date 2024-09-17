2adf38df9a7b71857db851a560d1f7e8a9b68883
AugustLoving <auglov536@ihmeducation.se>
2024-09-17 15:18:27 +0200

    p13 excercise buttons

--- P13HTMLDOM/js/app.js
+++ P13HTMLDOM/js/app.js
@@ -1,31 +1,12 @@
-// we reference "text0" and assign it to the variable called "textField0"
-const textfield0 = document.getElementById("text0");
-textfield0.innerHTML = average(20, 20);
-
-const headertext5 = document.getElementById("header5");
-headertext5.innerHTML = "This is my website!";
-
-headertext5.style.color = "pink";
-
-
-const button0 = document.getElementById("button0");
-
-button0.style.backgroundColor = "red";
-button0.style.color = "white";
-button0.style.borderColor = "blue";
-button0.style.cursor = "pointer";
-
-
-function multiplyBySelf(numberToMultiply) {
-  return numberToMultiply * numberToMultiply;
+let score = 0;
+function increaseBy(inputNumber) {
+  return score += inputNumber;
 }
 
-function add(firstNumber, secondNumber) {
-  return firstNumber + secondNumber;
-}
+const knappen = document.getElementById("button0");
 
-function average(Siffra1, Siffra2) {
-  return (Siffra1 + Siffra2) / 2;
-  }
+knappen.innerHTML = increaseBy(1);
+
+
 
 
