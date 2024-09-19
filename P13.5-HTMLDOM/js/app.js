dabbba58a7a92e1331c9cda21b32d3fab838661f
AugustLoving <auglov536@ihmeducation.se>
2024-09-19 10:47:32 +0200

    Refactor to improve modularity

--- P13.5-HTMLDOM/js/app.js
+++ P13.5-HTMLDOM/js/app.js
@@ -1,8 +1,15 @@
+// document elements
 const textField0 = document.getElementById("text0");
-textField0.innerHTML = showStylizedScore(786);
+const textField1 = document.getElementById("text1");
 
+// internal Variables
+let score = 0;
 
+// process,
 
+increaseScoreByOne()
+
+// Controllers
 function showScore(inputNumber) {
   return inputNumber * 100;
 }
@@ -11,4 +18,16 @@ function showStylizedScore(scoreInput) {
   return "your score is: " + scoreInput * 100;
 }
 
+function increaseScoreByOne() {
+  score++;
+  updateScoreText(score);
+}
 
+// View
+function updateScoreText(newText){
+  textField0.innerHTML = newText;
+}
+
+function updateStatusText(newText){
+  textField1.innerHTML = newText;
+}
