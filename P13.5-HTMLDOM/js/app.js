4d2bf850017b85fa4cce51af6fb247db751f316e
AugustLoving <auglov536@ihmeducation.se>
2024-09-19 12:36:11 +0200

    buttons and your score is text

--- P13.5-HTMLDOM/js/app.js
+++ P13.5-HTMLDOM/js/app.js
@@ -2,10 +2,28 @@
 const textField0 = document.getElementById("text0");
 const textField1 = document.getElementById("text1");
 
+
+//buttons
+const increaseScoreButton = document.getElementById("button0");
+const decreaseScoreButton = document.getElementById("button1");
+
+decreaseScoreButton.style.color = "red";
+increaseScoreButton.style.color = "green";
+
+
+
+
 // internal Variables
 let score = 0;
 
-// process,
+
+// button click
+// Process (what is going to happen in what sequence?)
+increaseScoreButton.addEventListener("click", () => {
+  increaseScoreByOne()
+  checkForSeven()
+
+});
 
 increaseScoreByOne()
 
@@ -23,11 +41,30 @@ function increaseScoreByOne() {
   updateScoreText(score);
 }
 
+function checkForSeven(){
+  if (score >= 7) {
+    changeTextToGreen();
+    }
+
+  }
+
+function changeTextToGreen(){
+  textField0.style.color = "green";
+}
+
 // View
 function updateScoreText(newText){
-  textField0.innerHTML = newText;
+  textField0.innerHTML = "Your Score Is: " + newText;
 }
 
 function updateStatusText(newText){
   textField1.innerHTML = newText;
 }
+
+// minus
+function decreaseScoreByOne() {
+  score--;
+  updateScoreText(score);
+}
+
+
