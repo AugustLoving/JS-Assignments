2a478bf72a012a02cbf058797e826a12650d0cb1
AugustLoving <auglov536@ihmeducation.se>
2024-09-19 10:34:24 +0200

    P13.5 idk

---
+++ P13.5-HTMLDOM/js/app.js
@@ -0,0 +1,14 @@
+const textField0 = document.getElementById("text0");
+textField0.innerHTML = showStylizedScore(786);
+
+
+
+function showScore(inputNumber) {
+  return inputNumber * 100;
+}
+
+function showStylizedScore(scoreInput) {
+  return "your score is: " + scoreInput * 100;
+}
+
+
